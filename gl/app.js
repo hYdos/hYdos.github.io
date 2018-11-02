1c0cb943fcd1d439badc06ee636a389895ee1b5a
Spigot-server <haydenv06@gmail.com>
2018-11-03 07:10:31 +1000

    umm this should work

--- gl/app.js
+++ gl/app.js
@@ -1,6 +1,6 @@
 
-var VertexShaderText =  $.get('vertexShader.glsl');
-var FragmentShaderText = $.get('fragmentShader.glsl');
+var VertexShaderText =  $.get('gl/vertexShader.glsl');
+var FragmentShaderText = $.get('gl/fragmentShader.glsl');
 
 
 
